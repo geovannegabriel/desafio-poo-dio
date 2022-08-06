bf380e40ad8711f3ad7cb4c3650fe97a3e4fc135
geovannegabriel <geovannegabriel29@hotmail.com>
2022-08-05 23:28:13 -0300

    curso att2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import br.com.dio.desafio.dominio.Conteudo;
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -22,9 +20,35 @@ public class Main {
         mentoria.setDescricao("Descricacao da mentoria");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome(("Bootcamp Java Developer"));
+        bootcamp.setDescricao(("Descrição Bootcamp Java Developer"));
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devGabriel = new Dev();
+        devGabriel.setNome("Gabriel");
+        devGabriel.increverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos" + devGabriel.getConteudosIncritos());
+        devGabriel.proguedir();
+        System.out.println("Conteudos Concluidos" + devGabriel.getConteudosConcluidos());
+
+
+
+        Dev devJose = new Dev();
+        devJose.setNome("José");
+        devJose.increverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos" + devJose.getConteudosIncritos());
+        devJose.proguedir();
+        System.out.println("Conteudos Concluidos" + devJose.getConteudosConcluidos());
+
+
+
 
 
     }
